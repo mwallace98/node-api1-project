1fb74ee4be632b154cededcfac85b5eae309d899
mwallace98 <mattwallace140@gmail.com>
2023-12-23 12:38:02 -0500

    get user by ID working

--- api/server.js
+++ api/server.js
@@ -16,6 +16,24 @@ server.get('/api/users', (req,res) => {
     })
 })
 
+server.get('/api/users/:id', (req,res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user){
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        }
+        console.log(user)
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            err: err.message
+        })
+    })
+})
+
 server.use('*', (req,res) => {
     res.status(404).json({
         message: 'not found'
